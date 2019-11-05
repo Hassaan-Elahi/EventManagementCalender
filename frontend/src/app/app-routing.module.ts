d83b3256b6aaa354228a4d6edecb1b373309f603
Hassaan Elahi <hassaan.elahi@conradlabs.com>
2019-11-05 09:51:39 +0500

    added login services

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {buildServePath} from '@angular-devkit/build-angular/src/dev-server';
+import {CalenderComponent} from './calender/calender.component';
+import {LoginComponent} from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalenderComponent } from './calender/calender.component';
+import {AgGridAngular, AgGridModule} from 'ag-grid-angular';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalenderComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
